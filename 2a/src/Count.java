400eb00d9377b960118e2ef991178ab916059ba4
Sarhad Maisoon Salam <sarhadmaisoon18@gmail.com>
2018-01-13 14:59:26 -0500

    2a complete

--- 2a/src/Count.java
+++ 2a/src/Count.java
@@ -6,24 +6,66 @@ import java.util.*;
  */
 public class Count
 {
+	private static final String datasetPath = "/home/sarhad/Projects/utek/comp-pack/ptb.train.txt";
 	
 	private static ArrayList<String> list = new ArrayList<>();
+	private static int count = 0;
 	
 	public static void main(String[] args)
 	{
 		try
 		{
 			readFile();
+			String key = "OR";
+			for( String aList : list )
+			{
+				countKeys(aList, key);
+			}
+			System.out.println(count);
+			
 		} catch( FileNotFoundException e )
 		{
 			e.printStackTrace();
 		}
 	}
 	
+	private static int countKeys(String data, String key)
+	{
+		if( data.length()<key.length() )
+		{
+			return -1;
+		}
+		for( int i = 0; i<( data.length()-key.length()+1 ); i++ )
+		{
+			if( data.charAt(i) == key.charAt(0) )
+			{
+				boolean flag = true;
+				for( int j = 0; j<key.length(); j++ )
+				{
+					if( data.charAt(i+j) == key.charAt(j) )
+					{
+						continue;
+					} else
+					{
+						flag = false;
+						break;
+					}
+				}
+				if( flag )
+				{
+					count += 1;
+				}
+			}
+		}
+		
+		return count;
+	}
+	
 	private static void readFile() throws FileNotFoundException
 	{
 		//Read Your File here. Use whatever helper functions you want. Pass the data line by line to processData(String s)
-		Scanner x = new Scanner(new File("/home/sarhad/Projects/utek/comp-pack/ptb.train.txt"));
+		
+		Scanner x = new Scanner(new File(datasetPath));
 		while( x.hasNext() )
 		{
 			String a = x.next();
@@ -40,7 +82,6 @@ public class Count
 		
 		String S[] = s.split(" ");
 		list.addAll(Arrays.asList(S));
-		System.out.println(s);
 	}
 	
 	private static void writeFile()
